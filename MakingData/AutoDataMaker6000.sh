aece3a87e33852c98169d9136f56df4834811563
hobo Wyatt <124455397+SurvivalW@users.noreply.github.com>
2026-02-06 19:53:25 -0500

    Done with Fake Data

--- MakingData/AutoDataMaker6000.sh
+++ MakingData/AutoDataMaker6000.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+while IFS= read -r line || [[ -n $line ]]; do
+    [[ $line == ID,* ]] && { echo "$line"; continue; }
+    Stock=$(shuf -i 1-100 -n 1)
+    sed "s/,$/,${Stock},/" <<< "$line"
+done < BaseData.csv > KrogerTest.csv
+
+while IFS= read -r line || [[ -n $line ]]; do
+    [[ $line == ID,* ]] && { echo "$line"; continue; }
+    Price=$(shuf -i 1-67 -n 1)
+    sed "s/,$/,${Price},/" <<< "$line"
+done < KrogerTest.csv
+
+rm KrogerTest.csv
